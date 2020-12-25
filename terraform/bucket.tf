befc8b181e4258110f54730d1187d755ba00a7d4
Danny Heard <dannyheard7@gmail.com>
2020-12-25 18:47:00 +0000

    tf fmt

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,8 +1,8 @@
 resource "google_storage_bucket" "frontend" {
   provider = google
-  name = "${var.project_id}/frontend"
+  name     = "${var.project_id}/frontend"
   location = "EUROPE-WEST2"
-} 
+}
 
 resource "google_storage_default_object_access_control" "website_read" {
   bucket = google_storage_bucket.frontend.name
