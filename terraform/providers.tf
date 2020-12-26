6372a3cb7d94cb67fde68d4e7447f3902236c490
Danny Heard <dannyheard7@gmail.com>
2020-12-26 17:21:39 +0000

    add backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,3 +2,9 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+terraform {
+  backend "gcs" {
+    bucket = "organic-area-299215-tfstate"
+    prefix = "root\tfsate"
+  }
+}
