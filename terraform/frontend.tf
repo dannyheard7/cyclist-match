8bb8f816b5872fc11aa79daa754a827c7d55c5f8
Danny Heard <dannyheard7@gmail.com>
2020-12-28 16:49:42 +0000

    Move frontend resources into single file

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,3 +1,41 @@
+# Bucket 
+
+resource "google_storage_bucket" "frontend" {
+  provider = google
+  name     = "${var.project_id}-frontend"
+  location = "EUROPE-WEST2"
+}
+
+resource "google_storage_default_object_access_control" "website_read" {
+  bucket = google_storage_bucket.frontend.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+
+# Reserve an external IP
+resource "google_compute_global_address" "website" {
+  provider = google
+  name     = "website-lb-ip"
+}
+
+# Get the managed DNS zone
+resource "google_dns_managed_zone" "gcp_cycling_buddies" {
+  provider = google
+  name     = "gcp-cycling-buddies"
+  dns_name = "elevait.co.uk."
+}
+
+# Add the IP to the DNS
+resource "google_dns_record_set" "website" {
+  provider     = google
+  name         = google_dns_managed_zone.gcp_cycling_buddies.dns_name
+  type         = "A"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.gcp_cycling_buddies.name
+  rrdatas      = [google_compute_global_address.website.address]
+}
+
 # Add the bucket as a CDN backend
 resource "google_compute_backend_bucket" "website" {
   provider    = google
