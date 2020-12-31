7ffcf30fdf7964d0ae1556fad159b9485357befa
Danny Heard <dannyheard7@gmail.com>
2020-12-31 10:06:19 +0000

    Add GCRs

---
+++ terraform/backend.tf
@@ -0,0 +1,22 @@
+# Container registries
+resource "google_container_registry" "api_registry" {
+  project  = var.project_id
+  location = "EU"
+}
+
+resource "google_storage_bucket_iam_member" "api_registry_terraform_sa_iam" {
+  bucket = google_container_registry.api_registry.id
+  role   = "roles/storage.admin"
+  member = "serviceAccount:terraform@${var.project_id}.iam.gserviceaccount.com"
+}
+
+resource "google_container_registry" "db_migration_registry" {
+  project  = var.project_id
+  location = "EU"
+}
+
+resource "google_storage_bucket_iam_member" "db_migration_registry_terraform_sa_iam" {
+  bucket = google_container_registry.db_migration_registry.id
+  role   = "roles/storage.admin"
+  member = "serviceAccount:terraform@${var.project_id}.iam.gserviceaccount.com"
+}
--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -2,7 +2,7 @@
 resource "google_storage_bucket" "frontend" {
   provider                    = google
   name                        = "wwww.elevait.co.uk"
-  location                    = "EUROPE-WEST2"
+  location                    = var.region
   uniform_bucket_level_access = true
 
   website {
