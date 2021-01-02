21727545a9ec2b1afebce3de541df81d09fd3b44
Danny Heard <dannyheard7@gmail.com>
2021-01-02 14:34:15 +0000

    Add healthcheck

---
+++ backend/RuntimeService/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RuntimeService.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetHealth()
+        {
+            return Ok();
+        }
+    }
+}
