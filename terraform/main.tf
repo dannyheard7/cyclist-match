9e4974d273ace7ef61417dfc4ae8114c2e5fe600
Danny Heard <danny.heard@airelogic.com>
2022-01-23 11:16:15 +0000

    Move terraform code into seperate repo?

--- terraform/main.tf
+++
@@ -1,7 +0,0 @@
-data "google_client_config" "current" {}
-
-data "google_client_openid_userinfo" "terraform_sa" {}
-
-locals {
-  terraform_sa_member = "serviceAccount:${data.google_client_openid_userinfo.terraform_sa.email}"
-}
